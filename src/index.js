9117dfdd483a9f56ce07bc37d97e79abdcea978e
ctb116 <cathytbradley116@gmail.com>
2022-09-15 21:37:43 -0700

    add info on React elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
 // gets the "root" DOM element from index.html and passes it to createRoot()
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const element = <h1>Hello, World</h1>;
 root.render(element);
+
+function tick() {
+  const element = (
+    <div>
+      <h1>hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element);
+}
+
+setInterval(tick, 1000);
+//root.render() is called every second from the setInterval() callback
+
+// This is just for practice to mess around with rendering elements. In most react applications there is just one root.render and React apps only call root.render() once. This is done by how code is encapsulated into stateful components later.
+
+// remember there needs to be a parent element so when createElement() is used there is only one arguement for the type value
+const elementTwo = (
+  <div>
+    <h1>React only updates what is necessary</h1>
+    <p>
+      React DOM compares the elements and its children to the previous elemenet
+      that was rendered and only updates what is nescessary. Can inspect element
+      to on the ticking clock to see. Only the text node whose content has
+      changed gets updated, meaning the div and the header containing hello
+      world are not updated. This is what makes React apps fast
+    </p>
+    <p>
+      from React "thinking how the UI should look at any given moment, rather
+      than how to change it over time, eliminates a whole class of bugs
+    </p>
+  </div>
+);
 
 // This is the basic render of a React element from the React Main Concepts docs, before components are introduced. StackBliz starts with more boiler template than needed to learn these concepts
 // https://reactjs.org/docs/rendering-elements.html
