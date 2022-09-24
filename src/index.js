a1d721d5c7d08554e5489b7163ca3f8d9d824536
ctb116 <cathytbradley116@gmail.com>
2022-09-23 21:58:12 -0700

    add info lifecycle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 import './style.css';
 
 // gets the "root" DOM element from index.html and passes it to createRoot()
@@ -58,6 +58,61 @@ class Bank extends React.Component {
 // Give an example what a class component could look like
 
 // This will likely get people mad because 25 now represents the amount and it is not clear where that number comes from
+
+// you need super() for "this" keyword to work.
+// you could make a class component without a constructor or the super() but it is not recommend. Why?
+  // It is fine to not have a constructor if you don't need to intialize state and you don't need to bind methods.
+  // a constructor is called before the component is unmounted
+
+
+
+// LIFECYCLE METHODS TO CLASS COMPONENTS
+// app will start to have many components. It is important to free up resources used by components when they are destroyed.
+
+// mounting - what happens when a componented in rendered in the DOM for the first time
+// unmounting - what happens when a component is removed from the DOM
+
+
+class BankTwo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: 50,
+      date: new Date()
+    };
+  }
+
+  //what happens when the component is rendered to the DOM for the first time
+  // from the React docs - not sure what this means:
+    // While this.props is set up by React itself and this.state has a special meaning, you are free to add additional fields to the class manually if you need to store something that doesn’t participate in the data flow (like a timer ID).
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+
+  }
+
+  // what happens when the component is removed from the DOM
+  // this and componentDidMount() are called lifecycle methods
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Amount is now {(this.state.account -= amount)}</p>
+      </div>
+    );
+  }
+}
 
 const infoPt1 = (
   <div>
